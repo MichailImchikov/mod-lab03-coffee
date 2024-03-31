465262781ce956ee7f75db54dad8455984203cfa
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-03-31 15:37:35 +0300

    Create Automata.cpp

---
+++ src/Automata.cpp
@@ -0,0 +1,55 @@
+#include "Automata.h" 
+#include <iostream>
+#include <map>
+#include <vector>using namespace std;
+Automata::Automata()
+{
+    cash = 0;
+    state = States::Off;
+};
+void Automata::On()
+{
+    if (state == States::Off) state = States::Wait;
+}
+void Automata::Off()
+{
+    state = States::Off;
+}
+void Automata::coin(int Cash)
+{
+    cash=Cash;
+    if (state = States::Wait) state = Accept;
+}
+void Automata::etMenu(vector<string> Menu,vector<float> Prices)
+{
+    Menu = menu;
+    Prices = prices;
+}
+States Automata::getState()
+{
+    return state;
+}
+void Automata::choice(string Drink)
+{
+    for( int i=0;i<menu.size();i++)
+    {
+        if (Drink == menu[i])
+        {
+            if (state == States::Accept) { state = States::Check; }
+            index = i;
+        }
+    }
+}
+void Automata::check()
+{
+    if (prices[index] < cash && state == States::Check) { state = States::Cook; }
+}
+float Automata::cook()
+{   
+    if(state = States::Cook) return prices[index] - cash;
+}
+string Automata::finish()
+{
+    state = States::Wait;
+    return menu[index];
+}
